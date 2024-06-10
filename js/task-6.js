1e3b787b02fa092cb545af1ac7b4211329ed4583
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-06-10 11:58:13 +0300

    10/06/24

--- js/task-6.js
+++ js/task-6.js
@@ -4,42 +4,44 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const amountInput = document.querySelector('#controls input');
+const boxesDiv = document.querySelector('#boxes');
 
 function createBoxes(amount) {
-  const boxesDiv = document.querySelector('#boxes');
-  boxesDiv.innerHTML = ''; // Очищаємо вміст перед створенням нових елементів
 
-  let size = 30;
-  for (let i = 0; i < amount; i++) {
-    const box = document.createElement('div');
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    box.style.marginBottom = '10px';
-    boxesDiv.appendChild(box);
-    size += 10;
-  }
+    destroyBoxes();
+
+    let size = 30;
+    for (let i = 0; i < amount; i++) {
+        const box = document.createElement('div');
+        box.style.width = `${size}px`;
+        box.style.height = `${size}px`;
+        box.style.backgroundColor = getRandomHexColor();
+        box.style.marginBottom = '10px';
+        boxesDiv.appendChild(box);
+        size += 10;
+    }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  const createButton = document.querySelector('[data-create]');
-  const destroyButton = document.querySelector('[data-destroy]');
-  const amountInput = document.querySelector('#controls input');
+function destroyBoxes(){
+	boxesDiv.innerHTML = '';
+}
 
-  createButton.addEventListener('click', function() {
-    const amount = parseInt(amountInput.value.trim(), 10);
+    createButton.addEventListener('click', function() {
+        const amount = parseInt(amountInput.value.trim(), 10);
 
-    if (isNaN(amount) || amount < 1 || amount > 100) {
-      alert('Please enter a number between 1 and 100.');
-      return;
-    }
+        if (isNaN(amount) || amount < 1 || amount > 100) {
+            alert('Please enter a number between 1 and 100.');
+            return;
+        }
 
-    createBoxes(amount);
-    amountInput.value = ''; // Очищаємо значення введення
-  });
+        createBoxes(amount);
+        amountInput.value = ''; // Очищаємо значення введення
+    });
 
-  destroyButton.addEventListener('click', function() {
-    const boxesDiv = document.querySelector('#boxes');
-    boxesDiv.innerHTML = ''; // Очищаємо контейнер з квадратами
-  });
-});
+    destroyButton.addEventListener('click', function() {
+        destroyBoxes();
+    });
+
